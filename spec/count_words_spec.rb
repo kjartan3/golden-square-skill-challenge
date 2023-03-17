2ef1d8c46f0bf794034b586427caefd1e0197bf4
kjartanh <kheimission@gmail.com>
2023-03-17 10:18:36 +0000

    Add and finish countwords file and spec

---
+++ spec/count_words_spec.rb
@@ -0,0 +1,17 @@
+require 'count_words'
+
+RSpec.describe "count_words method" do
+  context "when given a string" do
+    it "returns number of words" do
+      result = count_words("one two three four")
+      expect(result).to eq 4
+    end
+  end
+
+  context "when strings contains anything other than a word" do
+    it "raises an error" do
+        result = count_words("1")
+        expect(result).to eq "Please enter words only" 
+    end
+  end
+end
